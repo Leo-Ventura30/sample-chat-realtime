7deb2829fd5406532c6338a584f404bf311b39a6
leofventura98 <leonardo.ventura@outlook.com>
2021-10-08 18:27:57 -0300

    chore: define routes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-app.get('/', (req, res) => {
-  res.send("Entre na sala com um nome")
+app.get("/", (req, res) => {
+  return res.send("Entre na sala com um nome");
 });
 
 // io.on('connection', (socket) => {
@@ -17,28 +17,31 @@ app.get('/', (req, res) => {
 //     console.log('user disconnected');
 //   });
 // });
-let message = []
-let users = []
-app.get("/room?:name", (req,res)=>{
-  res.sendFile(__dirname + '/main.html');
-})
+let message = [];
+let users = [];
+app.get("/room?:name", (req, res) => {
+  return res.sendFile(__dirname + "/main.html");
+});
 
-io.on('connection', (socket) => {
-  socket.on('connectUser', (user) => {
-    users.forEach(userArr=>{
-      if(userArr ==user) {console.log("ja existe esse nome")}else{console.log("pode logar")}
-    })
-    users.push(user)
-    socket.broadcast.emit('connectedUser', user)
+io.on("connection", (socket) => {
+  socket.on("connectUser", (user) => {
+    users.forEach((userArr) => {
+      if (userArr == user) {
+        console.log("ja existe esse nome");
+      } else {
+        console.log("pode logar");
+      }
+    });
+    users.push(user);
+    socket.broadcast.emit("connectedUser", user);
   });
-  socket.on('sendMessage', (msg) => {
-    message.push(msg)
-    socket.broadcast.emit('receveidMessage', msg)
+  socket.on("sendMessage", (msg) => {
+    message.push(msg);
+    socket.broadcast.emit("receveidMessage", msg);
   });
-  console.log(users)
-
+  console.log(users);
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
